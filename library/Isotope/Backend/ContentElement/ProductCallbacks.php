e3027464562ebcd74fd082d1f7a60c276a8172cc
Adam Fisher <adam@rhyme.digital>
2017-07-13 15:36:04 -0400

    Improved backend product options

---
+++ library/Isotope/Backend/ContentElement/ProductCallbacks.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * Copyright (C) 2017 Rhyme Digital, LLC.
+ * 
+ * @link		http://rhyme.digital
+ * @license		http://www.gnu.org/licenses/lgpl-3.0.html LGPL
+ */
+
+namespace Isotope\Backend\ContentElement;
+
+
+class ProductCallbacks extends \Backend
+{
+	
+	/**
+	 * Run
+	 */
+	public function run()
+	{
+		$arrOptions = array();
+		
+		$objProducts = \Database::getInstance()->prepare("SELECT * FROM tl_iso_product")->execute();
+		if ($objProducts->numRows)
+		{
+			while ($objProducts->next())
+			{
+				$arrOptions[$objProducts->id] = $objProducts->name . ' (ID ' . $objProducts->id . ')';
+			}
+		}
+		
+		return $arrOptions;
+	}
+}
